4e4459141ae02cc7a476bc69a4e26a5c71a602c3
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-03-30 17:00:26 +0200

    POST Y POST VACIO

--- shortener.py
+++ shortener.py
@@ -20,7 +20,7 @@ class Shortener (webapp.webApp):
     with the web content."""
 
     # Declare and initialize content
-    content = {"a":"b", "c":"d"}
+    content = {"a": "b", "c": "d"}
 
     def parse(self, request):
         """Return the resource name (including /)"""
@@ -42,6 +42,9 @@ class Shortener (webapp.webApp):
         print("BODY = " + body)
 
         htmlbody = ""
+        httpcode = ""
+        url = ""
+        short = ""
 
         questionaire = """<h1>Shorten URL's App</h1>
                           <form action="" method="POST">
@@ -55,11 +58,28 @@ class Shortener (webapp.webApp):
 
         if resourceName == "/":
             httpcode = "200 OK"
-            htmlbody = "<html><body>" + questionaire + "<h2>List:<br></h2>"+str(self.content)[1:-1]+"</body></html>"
+            if method == "GET":
+                htmlbody = "<html><body>" + questionaire + "<h2>List:<br></h2>"+str(self.content)[1:-1]+"</body></html>"
+            elif method == "POST":
+                url = body.split("&", 2)[0].split("=", 2)[1]
+                short = body.split("&", 2)[1].split("=", 2)[1]
+                if url == "" or short == "":
+                    htmlbody = "<html><body><p>QUERY STRING ERROR</p></body></html>"
+                else:
+                    if url[4] == "http" or url[4] == "https":
+                        htmlbody = "<html><body><p><h2>Original URL: <a href=" + url + ">" + url + "</a></h2></p>" + \
+                                   "<p><h2>Shorten URL: <a href=" + url + ">" + short + "</a> </h2></p>" + \
+                                   "</body></html>"
+                        self.content[short] = url
+                    else:
+                        print("FALTA PROTOCOLO HTTP")
+
+                print("URL = " + url + ", SHORT = " + short)
         else:
             httpcode = "404 Not Found"
             error_message = "Not implemented yet"
             htmlbody = "<html><body>" + error_message + "</body></html>"
+
         """
         if method == "POST":
             self.content[resourceName] = body.split("=")[1]
