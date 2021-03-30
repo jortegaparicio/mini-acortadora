04ab9e39c1b6291e103e2f029b05af878a845552
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-03-30 19:38:55 +0200

    Falta por verificar errores

--- shortener.py
+++ shortener.py
@@ -11,16 +11,18 @@
 """
 
 import webapp
+from urllib.parse import unquote
 
 
-class Shortener (webapp.webApp):
+
+class Shortener(webapp.webApp):
     """Simple web application for managing content.
 
     Content is stored in a dictionary, which is intialized
     with the web content."""
 
     # Declare and initialize content
-    content = {"a": "b", "c": "d"}
+    content = {}
 
     def parse(self, request):
         """Return the resource name (including /)"""
@@ -46,53 +48,49 @@ class Shortener (webapp.webApp):
         url = ""
         short = ""
 
-        questionaire = """<h1>Shorten URL's App</h1>
-                          <form action="" method="POST">
-                            <p>Insert URL: <input type="text" name="url"></p>
-                            <p>Insert Short URL: <input type="text" name="short"></p>
-                            <p>
-                              <input type="submit" value="Send">
-                            </p>
-                          </form>
+        questionaire = """
+                        <html>
+
+                            <body>
+                            <h1>Shorten URL's App</h1>
+                                <form action="/" method="POST">
+                                    <p>Insert URL: <input type="text" name="url"></p>
+                                    <p>Insert Short URL: <input type="text" name="short"></p>
+                                    <p>
+                                    <input type="submit" value="Send">
+                                    </p>
+                                </form>
                         """
 
         if resourceName == "/":
             httpcode = "200 OK"
             if method == "GET":
-                htmlbody = "<html><body>" + questionaire + "<h2>List:<br></h2>"+str(self.content)[1:-1]+"</body></html>"
+                htmlbody = questionaire + "<h2>List:<br></h2>" + str(self.content)[1:-1] + "</body></html>"
             elif method == "POST":
-                url = body.split("&", 2)[0].split("=", 2)[1]
+                url = unquote(body.split("&", 2)[0].split("=", 2)[1])
                 short = body.split("&", 2)[1].split("=", 2)[1]
                 if url == "" or short == "":
                     htmlbody = "<html><body><p>QUERY STRING ERROR</p></body></html>"
                 else:
-                    if url[4] == "http" or url[4] == "https":
-                        htmlbody = "<html><body><p><h2>Original URL: <a href=" + url + ">" + url + "</a></h2></p>" + \
-                                   "<p><h2>Shorten URL: <a href=" + url + ">" + short + "</a> </h2></p>" + \
-                                   "</body></html>"
-                        self.content[short] = url
-                    else:
-                        print("FALTA PROTOCOLO HTTP")
-
-                print("URL = " + url + ", SHORT = " + short)
+                    if not url.startswith('http://') and not url.startswith('https://'):
+                        url = 'https://' + url
+                    htmlbody = '<html><head><meta charset="utf-8" /><title>Shorten URL\'s App</title></head>' + \
+                               "<body><p><h2>Original URL: <a href=" + url + ">" + url + "</a></h2></p>" + \
+                               "<p><h2>Shorten URL: <a href=" + url + ">" + short + "</a> </h2></p>" + \
+                               "</body></html>"
+                    self.content[short] = url
+                    print("URL = " + url + ", SHORT = " + short)
+            else:
+                httpcode = "501 Not Implemented (en-US)"
+                error_message = "Method not implemented on this server"
+                htmlbody = "<html><body>" + error_message + "</body></html>"
         else:
-            httpcode = "404 Not Found"
-            error_message = "Not implemented yet"
-            htmlbody = "<html><body>" + error_message + "</body></html>"
-
-        """
-        if method == "POST":
-            self.content[resourceName] = body.split("=")[1]
-
-        if resourceName in self.content.keys():
-            httpcode = "200 OK"
-            htmlbody = "<html><body>" + self.content[resourceName]
-        else:
-            httpcode = "404 Not Found"
-            error_message = "Not Found. Add it by yourself"
-            htmlbody = "<html><body>" + error_message
-
-        """
+            if method == "GET":
+                if resourceName[1:] in self.content:
+                    httpcode = "301 Moved Permanently"
+                    htmlbody = '<html><head><title>Redirection</title><meta http-equiv="refresh"' \
+                               'content="2;URL='+self.content[resourceName[1:]]+'" /></head><body>' \
+                               '<p>REDIRECTION.....</p></body></html>'
 
         return httpcode, htmlbody
 
