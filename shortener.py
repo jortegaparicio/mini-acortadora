42bb18de5d89acefce147ece32f6a419fd98afc2
Juan Antonio <ja.ortega.2017@alumnos.urjc.es>
2021-03-30 19:54:36 +0200

    Entregado

--- shortener.py
+++ shortener.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """
- contentApp class
- Simple web application for managing content
+ Shortener class
+ Simple web application for shortener URLs
+
+ Author: Juan Antonio Ortega Aparicio
 
  Copyright Jesus M. Gonzalez-Barahona, Gregorio Robles 2009-2015
  jgb, grex @ gsyc.es
@@ -13,19 +15,17 @@
 import webapp
 from urllib.parse import unquote
 
-
-
 class Shortener(webapp.webApp):
-    """Simple web application for managing content.
+    """Simple web application for rename URLs.
 
-    Content is stored in a dictionary, which is intialized
+    Content is stored in a dictionary, which is initialised
     with the web content."""
 
     # Declare and initialize content
     content = {}
 
     def parse(self, request):
-        """Return the resource name (including /)"""
+        """Return the method in use, the resource in the call and the body"""
         method = request.split(' ', 2)[0]
         resource = request.split(' ', 2)[1]
         body = request.split('\r\n\r\n', 2)[1]
@@ -33,10 +33,9 @@ class Shortener(webapp.webApp):
         return method, resource, body
 
     def process(self, parsedname):
-        """Process the relevant elements of the request.
-
-        Finds the HTML text corresponding to the resource name,
-        ignoring requests for resources not in the dictionary.
+        """Process the URL adding it to a dictionary where the short URL is the Key and the original URL is the value.
+        When the user get in the app our server will show a form where the user can archive the URL and the Short URL,
+        after that, user can call the URL writing the short URL as a resource
         """
         (method, resourceName, body) = parsedname
         print("METHOD = " + method)
@@ -50,23 +49,22 @@ class Shortener(webapp.webApp):
 
         questionaire = """
                         <html>
-
                             <body>
                             <h1>Shorten URL's App</h1>
                                 <form action="/" method="POST">
-                                    <p>Insert URL: <input type="text" name="url"></p>
-                                    <p>Insert Short URL: <input type="text" name="short"></p>
-                                    <p>
-                                    <input type="submit" value="Send">
-                                    </p>
-                                </form>
+                                <p>Insert URL: <input type="text" name="url"></p>
+                                <p>Insert Short URL: <input type="text" name="short"></p>
+                                <p><input type="submit" value="Send"></p>
+                            </form>
+                            "<h2>List:<br></h2>" 
                         """
 
         if resourceName == "/":
             httpcode = "200 OK"
             if method == "GET":
-                htmlbody = questionaire + "<h2>List:<br></h2>" + str(self.content)[1:-1] + "</body></html>"
+                htmlbody = questionaire + str(self.content)[1:-1] + "</body></html>"
             elif method == "POST":
+                # Getting URL and short URL from body
                 url = unquote(body.split("&", 2)[0].split("=", 2)[1])
                 short = body.split("&", 2)[1].split("=", 2)[1]
                 if url == "" or short == "":
@@ -89,8 +87,15 @@ class Shortener(webapp.webApp):
                 if resourceName[1:] in self.content:
                     httpcode = "301 Moved Permanently"
                     htmlbody = '<html><head><title>Redirection</title><meta http-equiv="refresh"' \
-                               'content="2;URL='+self.content[resourceName[1:]]+'" /></head><body>' \
+                               'content="2;URL=' + self.content[resourceName[1:]] + '" /></head><body>' \
                                '<p>REDIRECTION.....</p></body></html>'
+                else:
+                    httpcode = "404 Not Found"
+                    htmlbody = "<html>Error: Resource not available</html>"
+            else:
+                httpcode = "501 Not Implemented (en-US)"
+                error_message = "Method not implemented on this server"
+                htmlbody = "<html><body>" + error_message + "</body></html>"
 
         return httpcode, htmlbody
 
